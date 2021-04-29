2e14986cff7e2660b52b5036fa2999c3b750ea0a
markpadilla8 <78052733+markpadilla8@users.noreply.github.com>
2021-04-29 15:07:31 -0700

    finish structured english

---
+++ test_2.py
@@ -0,0 +1,14 @@
+
+
+
+"""
+The program is trying to display the number of grades, the average grade, and the
+percentage of grades that are above the average grades using
+The number of grades: 24
+Average grade: 83.25
+Percentage of grades above average: 54.17%
+
+Required also to write a main() function to initialize (kickstart) the application, and a
+calculate_percent_above_average() function to calculate the percentage of grades that
+are above the average grade. 
+"""
