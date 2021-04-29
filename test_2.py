d8aca9bc7f7bf8fd0f5c962cff2399ef50d0d3e5
markpadilla8 <78052733+markpadilla8@users.noreply.github.com>
2021-04-29 15:50:22 -0700

    finish pseudocode

--- test_2.py
+++ test_2.py
@@ -12,3 +12,25 @@ Required also to write a main() function to initialize (kickstart) the applicati
 calculate_percent_above_average() function to calculate the percentage of grades that
 are above the average grade. 
 """
+
+"""
+##Analyze grades on a final examination. 
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0    # number of grades above average
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2f}%"
+                    .format(100 * num / len(grades)))
+
+main():
+    initialize (kickstart)
+    calculate_percent_above_average()
+"""
